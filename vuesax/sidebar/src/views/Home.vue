ea663122726bafcd4f3b40653026330eb9fd4e75
simonmrog <simonmrog@gmail.com>
2021-04-02 12:40:55 -0500

    sidebar using vuesax

---
+++ vuesax/sidebar/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <vs-button @click="active = !active" color="primary"
+      >Open Sidebar</vs-button
+    >
+    <vs-sidebar :parent="home" default-index="1" color="primary" v-model="active">
+      <router-link>Hello</router-link>
+      <vs-sidebar-item>
+        Dashboard
+      </vs-sidebar-item>
+    </vs-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      active: false
+    };
+  }
+};
+</script>
